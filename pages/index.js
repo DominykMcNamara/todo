bc2e5cd94628999283a7b9b4e2329c0c58ee0007
DominykMcNamara <dominyksmith@gmail.com>
2022-09-21 20:00:41 -0400

    nextjs and tailwindcss are working

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import Layout, {siteTitle} from "../components/Layout";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Layout>
+  );
 }
