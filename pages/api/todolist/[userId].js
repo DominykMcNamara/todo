f39a346885c6787c9db241fbe3f0ddc15b6239d4
DominykMcNamara <dominyksmith@gmail.com>
2022-11-04 15:32:36 -0400

    began api routing

---
+++ pages/api/todolist/[userId].js
@@ -0,0 +1,14 @@
+import { supabase } from "../../../lib/supabaseClient";
+
+export const handler = async({ query: {userId}}, res) => {
+    try {
+        const todolist = await supabase.from('todos').select('name, id, user_id, completed').match({ user_id: userId})
+        if(todolist) {
+            res.status(200).json(todolist)
+        } else {
+            res.status(404).json({ message: `Todo list belonging to a user with the id of %{ userId } cannot be found`})
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
